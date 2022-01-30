1750e80eeba280c6accd3d504bb50b9aa5d89ac6
André Moura <andrefelipebarbosamoura@gmail.com>
2022-01-30 13:05:47 -0300

    feat: make enemies move in defenders direction

--- main.js
+++ main.js
@@ -6,8 +6,13 @@ canvas.height = 600;
 const cellSize = 100;
 const cellGap = 3;
 const gameGrid = [];
-const defenders = [];
+let defenders = [];
 let numberOfResources = 300;
+const enemies = [];
+const enemiesPosition = [];
+let enemiesInterval = 600;
+let frame = 0;
+let gameOver = false;
 //---------------MOUSE-------------------
 let mouse = {
   x: 10,
@@ -63,12 +68,12 @@ function createGrid() {
     }
   }
 }
-createGrid();
 function handleGameGrid() {
   gameGrid.forEach((grid) => {
     grid.draw();
   });
 }
+createGrid();
 //--------------GRID-----------------------
 //---------------DEFENDER------------------
 class Defender {
@@ -86,7 +91,7 @@ class Defender {
     ctx.fillStyle = "blue";
     ctx.fillRect(this.x, this.y, this.width, this.height);
     ctx.fillStyle = "gold";
-    ctx.font = "30px Arial";
+    ctx.font = "30px Orbitron";
     ctx.fillText(Math.floor(this.health), this.x + 15, this.y + 30);
   }
 }
@@ -110,18 +115,75 @@ canvas.addEventListener("click", () => {
 });
 
 function handleDefenders() {
-  defenders.forEach((defender) => {
+  defenders.forEach((defender, index) => {
     defender.draw();
+    enemies.forEach((enemy) => {
+      if (collision(defender, enemy)) {
+        enemy.movement = 0;
+        defender.health -= 0.2;
+      }
+      if (defender.health <= 0) {
+        let aliveDefenders = defenders.filter(
+          (defender) => defender.health > 0
+        );
+        defenders = aliveDefenders;
+        enemy.movement = enemy.speed;
+      }
+    });
   });
 }
-
 //---------------DEFENDER------------------
+//---------------ENEMIES-------------------
+class Enemy {
+  constructor(verticalPosition) {
+    this.x = canvas.width;
+    this.y = verticalPosition;
+    this.width = cellSize;
+    this.height = cellSize;
+    this.speed = Math.random() * 0.2 + 0.4;
+    this.movement = this.speed;
+    this.health = 100;
+    this.maxHealth = this.health;
+  }
+  update() {
+    this.x -= this.movement;
+  }
+  draw() {
+    ctx.fillStyle = "red";
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+    ctx.fillStyle = "black";
+    ctx.font = "30px Arial";
+    ctx.fillText(Math.floor(this.health), this.x + 15, this.y + 30);
+  }
+}
+
+function handleEnemies() {
+  enemies.forEach((enemy) => {
+    enemy.update();
+    enemy.draw();
+    if (enemy.x < 0) {
+      gameOver = true;
+    }
+  });
+  if (frame % enemiesInterval === 0) {
+    let verticalPosition = Math.floor(Math.random() * 5 + 1) * cellSize;
+    enemies.push(new Enemy(verticalPosition));
+    enemiesPosition.push(verticalPosition);
+    if (enemiesInterval > 120) enemiesInterval -= 50;
+  }
+}
+//--------------ENEMIES-------------------
 //---------------UTILITIES----------------
 
 function handleGameStatus() {
   ctx.fillStyle = "black";
-  ctx.font = "30px Arial";
+  ctx.font = "30px Orbitron";
   ctx.fillText("Resources: " + numberOfResources, 20, 55);
+  if (gameOver) {
+    ctx.fillStyle = "black";
+    ctx.font = "60px Orbitron";
+    ctx.fillText("GAME OVER", 135, 330);
+  }
 }
 
 function animate() {
@@ -130,8 +192,10 @@ function animate() {
   ctx.fillRect(0, 0, controlsBar.width, controlsBar.height);
   handleGameGrid();
   handleDefenders();
+  handleEnemies();
   handleGameStatus();
-  requestAnimationFrame(animate);
+  frame++;
+  if (!gameOver) requestAnimationFrame(animate);
 }
 animate();
 
