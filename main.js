8c8e247711cf08ce3b8677932d9b5b4d28c7668f
André Moura <andrefelipebarbosamoura@gmail.com>
2022-01-30 12:10:27 -0300

    feat: created Defeders

--- main.js
+++ main.js
@@ -6,19 +6,40 @@ canvas.height = 600;
 const cellSize = 100;
 const cellGap = 3;
 const gameGrid = [];
-
-const mouse = {
-  x: undefined,
-  y: undefined,
+const defenders = [];
+let numberOfResources = 300;
+//---------------MOUSE-------------------
+let mouse = {
+  x: 10,
+  y: 10,
   width: 0.1,
   height: 0.1,
 };
+
+let canvasPosition = canvas.getBoundingClientRect();
+canvas.addEventListener("mousemove", (e) => {
+  mouse = {
+    ...mouse,
+    x: e.x - canvasPosition.left,
+    y: e.y - canvasPosition.top,
+  };
+});
+
+canvas.addEventListener("mouseleave", () => {
+  mouse = {
+    ...mouse,
+    x: undefined,
+    y: undefined,
+  };
+});
+//-------------------MOUSE-------------------
 
 const controlsBar = {
   width: canvas.width,
   height: cellSize,
 };
 
+//--------------GRID--------------
 class Cell {
   constructor(x, y) {
     this.x = x;
@@ -28,8 +49,10 @@ class Cell {
   }
 
   draw() {
-    ctx.strokeStyle = "black";
-    ctx.strokeRect(this.x, this.y, this.width, this.height);
+    if (mouse.x && mouse.y && collision(this, mouse)) {
+      ctx.strokeStyle = "black";
+      ctx.strokeRect(this.x, this.y, this.width, this.height);
+    }
   }
 }
 
@@ -46,11 +69,66 @@ function handleGameGrid() {
     grid.draw();
   });
 }
+//--------------GRID-----------------------
+//---------------DEFENDER------------------
+class Defender {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.width = cellSize;
+    this.height = cellSize;
+    this.shooting = false;
+    this.health = 100;
+    this.projectiles = [];
+    this.timer = 0;
+  }
+  draw() {
+    ctx.fillStyle = "blue";
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+    ctx.fillStyle = "gold";
+    ctx.font = "30px Arial";
+    ctx.fillText(Math.floor(this.health), this.x + 15, this.y + 30);
+  }
+}
+
+canvas.addEventListener("click", () => {
+  const gridPositionX = mouse.x - (mouse.x % cellSize);
+  const gridPositionY = mouse.y - (mouse.y % cellSize);
+  if (gridPositionY < cellSize) return;
+  let defenderCost = 100;
+  if (numberOfResources >= defenderCost) {
+    defenders.push(new Defender(gridPositionX, gridPositionY));
+    numberOfResources -= defenderCost;
+  }
+});
+
+function handleDefenders() {
+  defenders.forEach((defender) => {
+    defender.draw();
+  });
+}
+
+//---------------DEFENDER------------------
 
 function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.fillStyle = "red";
   ctx.fillRect(0, 0, controlsBar.width, controlsBar.height);
   handleGameGrid();
+  handleDefenders();
   requestAnimationFrame(animate);
 }
 animate();
+
+function collision(first, second) {
+  if (
+    !(
+      first.x > second.x + second.width ||
+      first.x + first.width < second.x ||
+      first.y > second.y + second.height ||
+      first.y + first.height < second.y
+    )
+  ) {
+    return true;
+  }
+}
